5e1757bb8c0d0a7adfbab8460524dbc9bd2c79e2
nav-yuht <116895242+nav-yuht@users.noreply.github.com>
2024-05-04 16:08:45 +0200

    reseni.

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,53 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
+@RequestMapping("/vizitka")
 public class VizitkaController {
+    private final List<Vizitka> seznamVizitek = List.of(
+            new Vizitka ("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "Praha 1 110 00",
+                    "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+
+            new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Václavské náměstí 837/11", "Praha 1 110 00",
+                     null, "+420 800123456", "www.czechitas.cz"),
+
+            new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "Praha 1 110 00",
+                    "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+
+            new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "Praha 1 110 00",
+                    "mirka@czechitas.cs", null, "www.czechitas.cz"),
+
+            new Vizitka(null, "Czechitas z. s.", "Václavské náměstí 837/11", "Praha 1 110 00",
+                     null, null, "www.czechitas.cz"),
+
+            new Vizitka(null, "Czechitas z. s.", "Václavské náměstí 837/11", "Praha 1 110 00",
+                    null, null, "www.czechitas.cz")
+
+    );
     @GetMapping("/")
     public String seznam() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("seznam", seznamVizitek);
         return "seznam";
     }
 
-    @GetMapping("/detail")
-    public String detail() {
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("detail", seznamVizitek.get(id));
         return "detail";
     }
+
+
 }
